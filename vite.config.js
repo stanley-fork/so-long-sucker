9fab38dd5757561ed9e761ebd23b986f49ef3217
Lout <40863861+lout33@users.noreply.github.com>
2026-01-20 16:45:21 -0500

    gg

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         game: resolve(__dirname, 'game.html'),
         results: resolve(__dirname, 'results.html'),
+        blog: resolve(__dirname, 'blog.html'),
       },
     },
   },
